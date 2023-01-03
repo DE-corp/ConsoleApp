5c8e4f17789a2583ce42c4479660a11b377c2082
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-03 19:02:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Animal
+    {
+        private string name;
+        private float speed;
+        private int age;
+        private string color;
+
+        public Animal(string name, float speed, int age, string color)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.age = age;
+            this.color = color;
+
+            show();
+        }
+
+        private void show()
+        {
+            int days_age = this.age * 365;
+            Console.WriteLine($"{this.name} двигается со скоростью {this.speed} километров в час. Имеет {this.color} цвет и ему {days_age} дней.\n");
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal("Виталик", 12.32f, 12, "белый");
+            Animal dog = new Animal("Черли", 42.32f, 4, "каштановый");
+        }
+    }
+}
